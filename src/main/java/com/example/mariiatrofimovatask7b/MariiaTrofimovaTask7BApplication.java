e4d47e1403999e88c3757c7420e34bfc00eec9ca
Трофимова Мария Игоревна <21458615@sigma.sbrf.ru>
2023-11-10 16:58:51 +0300

    Initial commit

---
+++ src/main/java/com/example/mariiatrofimovatask7b/MariiaTrofimovaTask7BApplication.java
@@ -0,0 +1,13 @@
+package com.example.mariiatrofimovatask7b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariiaTrofimovaTask7BApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MariiaTrofimovaTask7BApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mariiatrofimovatask7b/MariiaTrofimovaTask7BApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mariiatrofimovatask7b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariiaTrofimovaTask7BApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
